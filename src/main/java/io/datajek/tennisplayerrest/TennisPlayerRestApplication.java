3adf2b56727a56c488657c874a9b549b9eb87185
Ginny-Dang <ginnydang0728@gmail.com>
2022-10-31 10:07:28 -0500

    Init repo

---
+++ src/main/java/io/datajek/tennisplayerrest/TennisPlayerRestApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.tennisplayerrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/datajek/tennisplayerrest/TennisPlayerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.tennisplayerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
